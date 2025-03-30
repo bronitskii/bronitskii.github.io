e0932862a8575e70e2971642d186ff765a405e2e
bronitskii <125276853+bronitskii@users.noreply.github.com>
2025-03-30 23:09:46 +0200

    quickfix

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,8 +123,7 @@ window.addEventListener('load', function () {
 		$nav_li.eq($nav_li.length / 2).addClass("is-middle");
 	}
 
-	var delay = 50,
-		locked = false;
+	var locked = false;
 
 	function hideArticleContent($article) {
 		$article.removeClass("active");
@@ -150,6 +149,7 @@ window.addEventListener('load', function () {
 
 	$main._show = function (id, initial) {
 		var $article = $main_articles.filter('#' + id);
+
 		if ($article.length === 0) return;
 
 		if (locked) return;
@@ -322,7 +322,7 @@ window.addEventListener('load', function () {
 		});
 	});
 
-	$body.on("click", function (event) {
+	$body.on("click", function () {
 		if ($body.hasClass("is-article-visible")) $main._hide(true);
 	});
 
@@ -363,278 +363,350 @@ window.addEventListener('load', function () {
 	$main.hide();
 	$main_articles.hide();
 
-	if (location.hash !== "" && location.hash !== "#")
+	if (location.hash !== "" && location.hash !== "#") {
 		$window.on("load", function () {
 			$main._show(location.hash.substr(1), true);
 		});
-})(jQuery);
-
-const galleryImgs = document.querySelectorAll(".gallery-item img");
-
-galleryImgs.forEach((img) => {
-	img.addEventListener("click", () => {
-	});
-});
-
-function openModal(modalId, imgSrc) {
-	const modal = document.getElementById(modalId);
-	const modalImg = modal.querySelector(".modal-content img");
-	const modalCaption = modal.querySelector(".caption");
-	const loadingIndicator = modal.querySelector(".loading-indicator");
-
-	modal.style.display = "block";
-	setTimeout(() => {
-		modal.style.transform = "scale(1)";
-		modal.style.opacity = 1;
-	}, 10);
-
-	modal.addEventListener('click', (e) => {
-		e.stopPropagation();
-	});
-
-	modalCaption.style.display = "none";
-	modalImg.style.display = "none";
-	loadingIndicator.style.display = "flex";
-
-	const newImg = new Image();
-	newImg.onload = function () {
-		loadingIndicator.style.display = "none";
-		modalImg.src = imgSrc;
-		modalImg.style.display = "block";
-		modalCaption.style.display = "block";
-	};
-	newImg.src = imgSrc;
-}
-
-function closeModal(modalId) {
-	const modal = document.getElementById(modalId);
-	modal.style.transform = "scale(0.95)";
-	modal.style.opacity = 0;
-	setTimeout(() => {
-		modal.style.display = "none";
-	}, 300);
-}
-
-class TextScramble {
-	constructor(el) {
-		this.el = el;
-		this.chars = "!<>-_\\/[]{}ß%&*-=___+-≈#$>@/";
-		this.update = this.update.bind(this);
 	}
 
-	setText(newText) {
-		const oldText = this.el.innerText;
-		const length = Math.max(oldText.length, newText.length);
-		const promise = new Promise((resolve) => (this.resolve = resolve));
-		this.queue = [];
-		for (let i = 0; i < length; i++) {
-			const from = oldText[i] || "";
-			const to = newText[i] || "";
-			const start = Math.floor(Math.random() * 40);
-			const end = start + Math.floor(Math.random() * 40);
-			this.queue.push({ from, to, start, end });
+	const galleryImgs = document.querySelectorAll(".gallery-item img");
+
+	// Removed empty event listener; functional handlers are below
+
+	function openModal(modalId, imgSrc) {
+		const modal = document.getElementById(modalId);
+		const modalImg = modal.querySelector(".modal-content img");
+		const modalCaption = modal.querySelector(".caption");
+		const loadingIndicator = modal.querySelector(".loading-indicator");
+
+		modal.style.display = "block";
+		setTimeout(() => {
+			modal.style.transform = "scale(1)";
+			modal.style.opacity = 1;
+		}, 10);
+
+		modal.addEventListener('click', (e) => {
+			e.stopPropagation();
+		});
+
+		modalCaption.style.display = "none";
+		modalImg.style.display = "none";
+		loadingIndicator.style.display = "flex";
+
+		const newImg = new Image();
+		newImg.onload = function () {
+			loadingIndicator.style.display = "none";
+			modalImg.src = imgSrc;
+			modalImg.style.display = "block";
+			modalCaption.style.display = "block";
+		};
+		newImg.src = imgSrc;
+	}
+
+	function closeModal(modalId) {
+		const modal = document.getElementById(modalId);
+		modal.style.transform = "scale(0.95)";
+		modal.style.opacity = 0;
+		setTimeout(() => {
+			modal.style.display = "none";
+		}, 300);
+	}
+
+	class TextScramble {
+		constructor(el) {
+			this.el = el;
+			this.chars = "!<>-_\\/[]{}ß%&*-=___+-≈#$>@/";
+			this.update = this.update.bind(this);
 		}
-		cancelAnimationFrame(this.frameRequest);
-		this.frame = 0;
-		this.update();
-		return promise;
-	}
 
-	update() {
-		let output = "";
-		let complete = 0;
-		for (let i = 0, n = this.queue.length; i < n; i++) {
-			let { from, to, start, end, char } = this.queue[i];
-			if (this.frame >= end) {
-				complete++;
-				output += to;
-			} else if (this.frame >= start) {
-				if (!char || Math.random() < 0.28) {
-					char = this.randomChar();
-					this.queue[i].char = char;
+		setText(newText) {
+			const oldText = this.el.innerText;
+			const length = Math.max(oldText.length, newText.length);
+			const promise = new Promise((resolve) => (this.resolve = resolve));
+			this.queue = [];
+			for (let i = 0; i < length; i++) {
+				const from = oldText[i] || "";
+				const to = newText[i] || "";
+				const start = Math.floor(Math.random() * 40);
+				const end = start + Math.floor(Math.random() * 40);
+				this.queue.push({ from, to, start, end });
+			}
+			cancelAnimationFrame(this.frameRequest);
+			this.frame = 0;
+			this.update();
+			return promise;
+		}
+
+		update() {
+			let output = "";
+			let complete = 0;
+			for (let i = 0, n = this.queue.length; i < n; i++) {
+				let { from, to, start, end, char } = this.queue[i];
+				if (this.frame >= end) {
+					complete++;
+					output += to;
+				} else if (this.frame >= start) {
+					if (!char || Math.random() < 0.28) {
+						char = this.randomChar();
+						this.queue[i].char = char;
+					}
+					output += `<span class="dud">${char}</span>`;
+				} else {
+					output += from;
 				}
-				output += `<span class="dud">${char}</span>`;
+			}
+			this.el.innerHTML = output;
+			if (complete === this.queue.length) {
+				this.resolve();
 			} else {
-				output += from;
+				this.frameRequest = requestAnimationFrame(this.update);
+				this.frame++;
 			}
 		}
-		this.el.innerHTML = output;
-		if (complete === this.queue.length) {
-			this.resolve();
-		} else {
-			this.frameRequest = requestAnimationFrame(this.update);
-			this.frame++;
+
+		randomChar() {
+			return this.chars[Math.floor(Math.random() * this.chars.length)];
 		}
 	}
 
-	randomChar() {
-		return this.chars[Math.floor(Math.random() * this.chars.length)];
+	const phrases = [
+		"Professional Interpreter",
+		"Graphic Design Creator and InfoSec Enthusiast.",
+	];
+
+	const el = document.querySelector(".stext");
+	const fx = new TextScramble(el);
+
+	let counter = 0;
+	const next = () => {
+		fx.setText(phrases[counter]).then(() => {
+			setTimeout(next, 2500);
+		});
+		counter = (counter + 1) % phrases.length;
+	};
+
+	next();
+
+	// Canvas setup
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+	var minDistance = 20;
+	var characters = [];
+	var animationFrameId;
+	var animationRunning = true;
+	var lastFrameTime = 0;
+	// Reduce frame interval for smoother animation
+	var frameInterval = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i) ? 150 : 70;
+	var maxCharacters = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i) ? 40 : 120;
+
+	function setCanvasDimensions() {
+		// Get device pixel ratio
+		const dpr = window.devicePixelRatio || 1;
+
+		// Set canvas size properly accounting for device pixel ratio
+		canvas.width = window.innerWidth * dpr;
+		canvas.height = window.innerHeight * dpr;
+
+		// Set display size (css pixels)
+		canvas.style.width = window.innerWidth + 'px';
+		canvas.style.height = window.innerHeight + 'px';
+
+		// Scale context to counter the device pixel ratio
+		ctx.scale(dpr, dpr);
+
+		// Clear the canvas when resizing
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+		// Set font with proper size
+		var font_size = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i) ? 9 : 13;
+		ctx.font = font_size + "px 'JetBrains Mono', monospace";
+		ctx.textBaseline = 'top'; // Improves text placement consistency
+		ctx.imageSmoothingEnabled = false; // For crisp text
 	}
-}
 
-const phrases = [
-	"Professional Interpreter",
-	"Graphic Design Creator and InfoSec Enthusiast.",
-];
-
-const el = document.querySelector(".stext");
-const fx = new TextScramble(el);
-
-let counter = 0;
-const next = () => {
-	fx.setText(phrases[counter]).then(() => {
-		setTimeout(next, 2500);
-	});
-	counter = (counter + 1) % phrases.length;
-};
-
-next();
-
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-
-var minDistance = 20;
-
-window.onload = function () {
-	var blurAmount = 10;
-	var brightnessAmount = 0;
-	var intervalId = setInterval(function () {
-		if (blurAmount < 0.1 && brightnessAmount > 50) {
-			clearInterval(intervalId);
-		} else {
-			blurAmount -= 0.1;
-			brightnessAmount += 0.5;
-			canvas.style.filter = `brightness(${brightnessAmount}%) blur(${blurAmount}px)`;
-		}
-	}, 30);
-};
-
-function setCanvasDimensions() {
-	canvas.height = window.innerHeight;
-	canvas.width = window.innerWidth;
-}
-
-setCanvasDimensions();
-window.addEventListener("resize", setCanvasDimensions);
-
-var characters = [];
-var font_size = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i) ? 9 : 13;
-ctx.font = font_size + "px Consolas, monospace";
-
-function intersect(x1, y1, x2, y2) {
-	var distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-	return distance < minDistance;
-}
-
-function getRandomPosition() {
-	var x, y, intersecting;
-	do {
-		x = Math.floor(Math.random() * canvas.width);
-		y = Math.floor(Math.random() * canvas.height);
-		intersecting = false;
-		for (var i = 0; i < characters.length; i++) {
-			if (intersect(x, y, characters[i].x, characters[i].y)) {
-				intersecting = true;
-				break;
-			}
-		}
-	} while (intersecting);
-	return { x: x, y: y };
-}
-
-function draw() {
-	ctx.fillStyle = "rgba(0, 0, 0, 0.01)";
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-	ctx.fillStyle = "#00AA00";
-	ctx.font = font_size + "px Arial";
-
-	var text, opacity, position;
-	for (var i = 0; i < 10; i++) {
-		text = Math.random() < 0.5 ? "0" : "1";
-		position = getRandomPosition();
-		opacity = 0;
-		ctx.globalAlpha = opacity;
-		characters.push({ x: position.x, y: position.y });
-		(function (x, y, text, opacity) {
-			setTimeout(function () {
-				var fadeInterval = setInterval(function () {
-					if (opacity >= 0.481) {
-						clearInterval(fadeInterval);
-					} else {
-						opacity += 0.95;
-						ctx.globalAlpha = opacity;
-						ctx.fillText(text, x, y);
-					}
-				}, 50);
-			}, Math.random() * 2000);
-		})(position.x, position.y, text, opacity);
+	function intersect(x1, y1, x2, y2) {
+		var distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		return distance < minDistance;
 	}
-}
 
-var intervalTime = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i) ? 500 : 300;
-var intervalId = setInterval(draw, intervalTime);
-
-var timeoutTime = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i) ? 6000 : 6000;
-setTimeout(function () {
-	clearInterval(intervalId);
-}, timeoutTime);
-
-var galleryImages = document.querySelectorAll('.gallery-item img');
-var imageModals = document.querySelectorAll('[id^="myModal"]');
-var closeButtons = document.querySelectorAll('[id^="myModal"] .close');
-
-galleryImages.forEach(function (img, index) {
-	img.onclick = function () {
-		var modal = document.getElementById('myModal' + (index + 1));
-		if (modal) {
-			const loadingText = modal.querySelector('.loading-text');
-			const imgSrc = img.getAttribute('data-imgsrc');
-
-			loadingText.innerHTML = '[v:~]$ ';
-
-			async function typeWriterEffect(text) {
-				for (let i = 0; i < text.length; i++) {
-					if (text[i] === ' ') {
-						loadingText.innerHTML += '&nbsp;';
-					} else {
-						loadingText.innerText += text[i];
-					}
-					await new Promise(resolve => setTimeout(resolve, Math.random() * 50));
+	function getRandomPosition() {
+		var x, y, intersecting;
+		do {
+			x = Math.floor(Math.random() * window.innerWidth);
+			y = Math.floor(Math.random() * window.innerHeight);
+			intersecting = false;
+			for (var i = 0; i < characters.length; i++) {
+				if (intersect(x, y, characters[i].x, characters[i].y)) {
+					intersecting = true;
+					break;
 				}
 			}
+		} while (intersecting);
+		return { x, y };
+	}
 
-			typeWriterEffect("curl -LO " + new URL(window.location.href).origin + '/' + imgSrc);
-			openModal('myModal' + (index + 1), this.getAttribute('data-imgsrc'));
+	function addCharacters() {
+		// Only add characters if we're under the limit
+		if (characters.length < maxCharacters) {
+			// Add more characters at once for a more dynamic feel
+			var charsToAdd = Math.min(5, maxCharacters - characters.length);
+			for (var i = 0; i < charsToAdd; i++) {
+				var text = Math.random() < 0.5 ? "0" : "1";
+				var position = getRandomPosition();
+				characters.push({
+					x: position.x,
+					y: position.y,
+					text: text,
+					opacity: 0,
+					state: 'fading_in',
+					timer: 0
+				});
+			}
 		}
 	}
-});
 
-closeButtons.forEach(function (btn) {
-	btn.onclick = function (event) {
-		event.stopPropagation();
-		var modal = this.closest('[id^="myModal"]');
-		if (modal) {
-			closeModal(modal.id);
+	function draw(timestamp) {
+		// Request next frame immediately to ensure smooth animation
+		animationFrameId = requestAnimationFrame(draw);
+
+		// Calculate delta time for frame rate independence
+		if (!lastFrameTime) lastFrameTime = timestamp;
+		const deltaTime = timestamp - lastFrameTime;
+
+		// Only render at specified interval
+		if (deltaTime >= frameInterval) {
+			lastFrameTime = timestamp;
+
+			// Clear canvas with slight trail effect (more transparent for smoother trails)
+			ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+			ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+
+			// Update and remove characters
+			for (var i = characters.length - 1; i >= 0; i--) {
+				var char = characters[i];
+
+				if (char.state === 'fading_in') {
+					char.opacity += 0.3; // Faster fade in
+					if (char.opacity >= 1) {
+						char.opacity = 1;
+						char.state = 'visible';
+						char.timer = 5; // Very short display time (5 frames)
+					}
+				} else if (char.state === 'visible') {
+					char.timer -= 1;
+					if (char.timer <= 0) {
+						char.state = 'fading_out';
+					}
+				} else if (char.state === 'fading_out') {
+					char.opacity -= 0.3; // Faster fade out
+					if (char.opacity <= 0) {
+						// Remove character from array
+						characters.splice(i, 1);
+						continue; // Skip drawing this character
+					}
+				}
+
+				// Draw character with crisp rendering
+				ctx.fillStyle = `rgba(107, 245, 73, ${char.opacity})`;
+				ctx.fillText(char.text, char.x, char.y);
+			}
+
+			// Add new characters more frequently
+			if (animationRunning) {
+				addCharacters();
+			}
 		}
 	}
-});
 
-function updateProgressBar(article) {
-	const progressBar = article.querySelector('.progress-bar');
-	if (!progressBar) return;
+	// Initialize canvas and start animation
+	setCanvasDimensions();
+	window.addEventListener("resize", setCanvasDimensions);
+	animationFrameId = requestAnimationFrame(draw);
 
-	const scrollElement = article.querySelector('.scrollbar, .container');
-	const scrollPosition = scrollElement.scrollTop;
-	const scrollHeight = scrollElement.scrollHeight;
-	const clientHeight = scrollElement.clientHeight;
+	// Cleanup function to stop animation when navigating away
+	function stopAnimation() {
+		if (animationFrameId) {
+			cancelAnimationFrame(animationFrameId);
+			animationFrameId = null;
+		}
+		characters = [];
+	}
 
-	const scrolled = (scrollPosition / (scrollHeight - clientHeight)) * 100;
-	progressBar.style.width = scrolled + '%';
-}
+	// Add event listener to clean up animation when leaving page
+	window.addEventListener('beforeunload', stopAnimation);
 
-document.querySelectorAll('#more .scrollbar, #gallery .container').forEach(element => {
-	element.addEventListener('scroll', () => {
-		updateProgressBar(element.closest('article'));
-	});
-});
+	// Blur and brightness effect
+	window.onload = function () {
+		var blurAmount = 10;
+		var brightnessAmount = 0;
+		var blurIntervalId = setInterval(function () {
+			if (blurAmount < 0.1 && brightnessAmount > 50) {
+				clearInterval(blurIntervalId);
+			} else {
+				blurAmount -= 0.1;
+				brightnessAmount += 0.5;
+				canvas.style.filter = `brightness(${brightnessAmount}%) blur(${blurAmount}px)`;
+			}
+		}, 50);
+
+		// Gallery image handling
+		var galleryImages = document.querySelectorAll('.gallery-item img');
+		var imageModals = document.querySelectorAll('[id^="myModal"]');
+		var closeButtons = document.querySelectorAll('[id^="myModal"] .close');
+
+		galleryImages.forEach(function (img, index) {
+			img.onclick = function () {
+				var modal = document.getElementById('myModal' + (index + 1));
+				if (modal) {
+					const loadingText = modal.querySelector('.loading-text');
+					const imgSrc = img.getAttribute('data-imgsrc');
+
+					loadingText.innerHTML = '[v:~]$ ';
+
+					async function typeWriterEffect(text) {
+						for (let i = 0; i < text.length; i++) {
+							if (text[i] === ' ') {
+								loadingText.innerHTML += ' ';
+							} else {
+								loadingText.innerText += text[i];
+							}
+							await new Promise(resolve => setTimeout(resolve, Math.random() * 50));
+						}
+					}
+
+					typeWriterEffect("curl -LO " + new URL(window.location.href).origin + '/' + imgSrc);
+					openModal('myModal' + (index + 1), this.getAttribute('data-imgsrc'));
+				}
+			}
+		});
+
+		closeButtons.forEach(function (btn) {
+			btn.onclick = function (event) {
+				event.stopPropagation();
+				var modal = this.closest('[id^="myModal"]');
+				if (modal) {
+					closeModal(modal.id);
+				}
+			}
+		});
+
+		function updateProgressBar(article) {
+			const progressBar = article.querySelector('.progress-bar');
+			if (!progressBar) return;
+
+			const scrollElement = article.querySelector('.scrollbar, .container');
+			const scrollPosition = scrollElement.scrollTop;
+			const scrollHeight = scrollElement.scrollHeight;
+			const clientHeight = scrollElement.clientHeight;
+
+			const scrolled = (scrollPosition / (scrollHeight - clientHeight)) * 100;
+			progressBar.style.width = scrolled + '%';
+		}
+
+		document.querySelectorAll('#more .scrollbar, #gallery .container').forEach(element => {
+			element.addEventListener('scroll', () => {
+				updateProgressBar(element.closest('article'));
+			});
+		});
+	}
+})(jQuery);
